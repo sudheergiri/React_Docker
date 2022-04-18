93fc703463051b9f4a725807ac53755cca7d4943
gosalapradeep <79558129+gosalapradeep@users.noreply.github.com>
2022-04-18 13:41:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Gosala Pradeep making the changes.
+          This is Gosala Pradeep making the changes and pushing the code.
         </p>
         <a
           className="App-link"
