180d39980c64a6bb8a5e0851d395071f9f386973
gosalapradeep <79558129+gosalapradeep@users.noreply.github.com>
2022-04-18 15:02:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Gosala Pradeep commiting the changes .
+          This is Gosala Pradeep commiting the changes using pipeline.
         </p>
         <a
           className="App-link"
