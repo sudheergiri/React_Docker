4c61cfec666ed2bcebb8f03d35d0923457f47b61
gosalapradeep <79558129+gosalapradeep@users.noreply.github.com>
2022-04-18 15:27:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Gosala Pradeep making the changes using the jenkins Pipeline from Github.
+          This is Gosala Pradeep.
         </p>
         <a
           className="App-link"
