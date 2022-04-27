7204ccb6a588a6edd05c6bb8b3d6ff393a9c4b40
gosalapradeep <79558129+gosalapradeep@users.noreply.github.com>
2022-04-27 20:12:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is react application from Docker and then to the K8scluster .
+          This is sudheer pushing the code .
         </p>
         <a
           className="App-link"
