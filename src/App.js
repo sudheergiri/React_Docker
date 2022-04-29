443a0f5a61c63149a56712078e4141476a9b552d
sudheergiri <102008317+sudheergiri@users.noreply.github.com>
2022-04-29 12:59:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          react app from docker.
+          react app.
         </p>
         <a
           className="App-link"
